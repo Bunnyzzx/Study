c58d89d5e20b4ed64435e7b3fd07f0af35848ca0
Caio <121897609+Bunnyzzx@users.noreply.github.com>
2026-05-20 14:03:24 -0300

    Implement currency conversion functions
    Added conversion functions for Swiss Franc and Argentine Peso.

--- conversor_de_moedas.py
+++ conversor_de_moedas.py
@@ -2,7 +2,16 @@ def conversor_euro(a):
     return a / 5.77 
 
 def conversor_dolar(a):
+    return a / 4.93
+
+def conversor_franco_suico(a):
+    return a / 6.38
+
+def conversor_dolar(a):
     return a / 4.93 
+
+def conversor_peso_argentino(a):
+    return a* * 279
 
 
 
@@ -14,3 +23,9 @@ if escolha.lower() == "dolar":
 
 elif escolha.lower() == "euro": 
     print(f"Em Euro: € {conversor_euro(dinheiro):.2f}")
+
+elif escolha.lower() == "franco suico": 
+    print(f"Em Franco Suiço:  {conversor_franc0_suico(dinheiro):.2f}")
+
+elif escolha.lower() == "peso argentino": 
+    print(f"Em Peso Argentino:  {conversor_peso_argentino(dinheiro):.2f}")
